b497f375d4bec922116055cd51aa9c5ef6100c82
keerthana2624 <keerthanamummy2002@gmail.com>
2024-07-15 17:26:48 +0530

    modified app.js with required components

--- my-dashboard/src/App.js
+++ my-dashboard/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import StudentLogin from './components/StudentLogin';
+import StudentRegister from './components/StudentRegister';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/student-login" component={StudentLogin} />
+          <Route path="/student-register" component={StudentRegister} />
+          {/* Add routes for admin login and register if necessary */}
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
