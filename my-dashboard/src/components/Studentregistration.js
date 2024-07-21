b7f28caacb4c335a375cea2d2b51feb750ccb8e2
keerthana2624 <keerthanamummy2002@gmail.com>
2024-07-21 15:02:06 +0530

    implimented the student registration form

---
+++ my-dashboard/src/components/Studentregistration.js
@@ -0,0 +1,57 @@
+// src/components/Registration.js
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Registration = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = { email, password };
+
+    try {
+      const response = await fetch('http://localhost:5000/api/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        alert('Registration successful!');
+        navigate('/login');
+      } else {
+        alert('Registration failed. Please try again.');
+      }
+    } catch (error) {
+      console.error('Error registering:', error);
+      alert('Registration failed. Please try again later.');
+    }
+  };
+
+  return (
+    <div className="registration-container">
+      <div className="registration-card">
+        <h2>Registration Form</h2>
+        <form className="registration-form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+        <p className="login-helper-text">
+          Already have an account? <Link to="/login">Login here</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Registration;
