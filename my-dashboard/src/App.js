dcdd533b8aa0d8b4eed543aacec8b62de64c9754
keerthana2624 <keerthanamummy2002@gmail.com>
2024-07-25 10:44:51 +0530

    created the file as LandingPage

--- my-dashboard/src/App.js
+++ my-dashboard/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';  // Import the new component
 import StudentLogin from './components/StudentLogin';
 import Studentregistration from './components/Studentregistration';
 import Programs from './components/Programs';
 import Application from './components/Application';
+import AdminLogin from './components/Adminlogin';
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<StudentLogin />} />
+          <Route path="/" element={<LandingPage />} />  {/* Set LandingPage as the root route */}
+          <Route path="/student-login" element={<StudentLogin />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
           <Route path="/register" element={<Studentregistration />} />
           <Route path="/programs" element={<Programs />} />
-          <Route path="/apply" element ={<Application/>} />
+          <Route path="/apply" element={<Application />} />
         </Routes>
       </div>
     </Router>
